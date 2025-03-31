ac9daadd4ca139b9df2a06ce6156937d48ec8aeb
karar-git <addfgh177@gmail.com>
2025-03-31 20:03:44 +0300

    some edits

--- models/various_preprocessing/similarity_preprocessing.py
+++ models/various_preprocessing/similarity_preprocessing.py
@@ -2,15 +2,15 @@ import numpy as np
 from sklearn.preprocessing import OneHotEncoder
 from sentence_transformers import SentenceTransformer
 import pandas as pd
+import tensorflow as tf
+from torch import norm
+
 pd.set_option('display.max_columns', None)
 pd.set_option('display.max_colwidth', None)
 # Load the model
 my_vect_model = SentenceTransformer("paraphrase-MiniLM-L12-v2")
 # Load JSON data
 data = pd.read_json('./combined_dataset.json').copy()
-#modelly = upload it 
-#mlb = upload it 
-normalization_layer = tf.keras.layers.Normalization()
 def pipe_for_sim(data):
     data['description'] = 'title: ' + data['course_name'] + " | description: "+ data['description']
     data.drop(columns=['url', 'course_name','organization', 'instructor', 'subject', 'has_subject', 'reviews'],inplace = True)
@@ -40,9 +40,9 @@ def pipe_for_sim(data):
         return rating
     data['rating'] = data['rating'].apply(change)
     data['description'] = my_vect_model.encode(data['description']).tolist()
-    #data['skills'] = mlb.transform(data['skills'])
-    #data['skills'] = modelly.encode('skills')
-    #data['skills'] = np.mean(data['skills'], axis = 1)
+    data['skills'] = mlb_skill.transform(data['skills'])
+    data['skills'] = encoder_skill(data['skills'])
+    data['skills'] = np.mean(data['skills'], axis = 1)
     lists= ['description', 'level', 'provider', 'type', 'skills']
     def conca(row):
         # Convert each list-like feature to a NumPy array
@@ -55,21 +55,15 @@ def pipe_for_sim(data):
 normalization_layer = tf.keras.layers.Normalization()
 def all_data(data):
     dataa = pipe_for_sim(data)
-    global normalization_layer.adapt(dataa)
+    normalization_layer = tf.keras.layers.Normalization()
+    normalization_layer.adapt(dataa)
+    tf.saved_model.save(,normalization_layer, 'norm_layer')
     encoded_data = normalization_layer(dataa)
+
     return encoded_data
 def onecourse_(data):
     the_vector=pipe_for_sim(data)
     encoded_data = normalization_layer(the_vector)
 def save_norm_lay():
     pass
-AWHAT = pipe_for_sim(data)
-karar = AWHAT.iloc[2]
-karar
-karar.shape
-karar
-
-
-    
-
-
+#AWHAT = pipe_for_sim(data)
